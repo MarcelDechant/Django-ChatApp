345b0815bcdd08d999ce84f49a2134648c49df1e
Marcel Dechant <marcel.dechant@freenet.de>
2024-08-08 11:14:35 +0200

    first commit

---
+++ chat/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+# from django.utils import timezone
+
+
+
+# class Book(models.Model):
+
+#   title = models.CharField(max_length=200)
+
+#   author = models.CharField(max_length=100)
+
+#   publish_date = models.DateField(default=timezone.now)
+
+#   isbn = models.CharField(max_length=13, unique=True)
+
+#   page_count = models.IntegerField()
+
+#   description = models.TextField(blank=True)
+
+
+
+#   def __str__(self):
+
+#     return self.title
