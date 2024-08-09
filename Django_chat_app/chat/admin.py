85fa52bd7d3b8a0e395a5f4a1b580f9655a2725b
Marcel Dechant <marcel.dechant@freenet.de>
2024-08-09 16:18:46 +0200

    View_chat_messages

--- Django_chat_app/chat/admin.py
+++ Django_chat_app/chat/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from chat.models import Message
+from .models import Chat, Message
 
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ("text", "created_at", "author", "receiver")
+    fields = ("chat","text", "created_at", "author", "receiver")
     list_display = ("created_at", "author", "text", "receiver")
-    search_fields = ('text',)
-    
+    search_fields = ("text",)
 
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register( Chat)
--- Django_chat_app/chat/views.py
+++ Django_chat_app/chat/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 
 # Create your views here.
 def index(request):
-    return render(request, "chat/index.html", {"username": "Marcel"})
+    if request.method == "POST":
+        print("Received data " + request.POST["textmessage"])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST["textmessage"],
+            chat=myChat,
+            author=request.user,
+            receiver=request.user,
+        )
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, "chat/index.html", {"messages": chatMessages})
